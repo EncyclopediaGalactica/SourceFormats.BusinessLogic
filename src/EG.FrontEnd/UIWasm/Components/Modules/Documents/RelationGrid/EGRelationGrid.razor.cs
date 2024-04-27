6f79139458c009594e7d535d9f2a4489ee0db1b4
Andras Csanyi <andras.csanyi@pm.me>
2024-04-27 11:00:28 +0200

    grid

--- src/EG.FrontEnd/UIWasm/Components/Modules/Documents/RelationGrid/EGRelationGrid.razor.cs
+++ src/EG.FrontEnd/UIWasm/Components/Modules/Documents/RelationGrid/EGRelationGrid.razor.cs
@@ -1,7 +1,27 @@
+using EncyclopediaGalactica.BusinessLogic.Contracts;
 using Microsoft.AspNetCore.Components;
+using Microsoft.FluentUI.AspNetCore.Components;
+using UIWasm.Services;
 
 namespace UIWasm.Components.Modules.Documents.RelationGrid;
 
-public partial class EGRelationGrid : ComponentBase
+public partial class EGRelationGrid
 {
+    [Inject]
+    private ILogger<EGRelationGrid> Logger { get; set; }
+
+    [Inject]
+    private IRelationService RelationService { get; set; }
+
+    private FluentDataGrid<RelationResult> Grid;
+    private GridItemsProvider<RelationResult> GridItemsProvider;
+
+    protected async override Task OnInitializedAsync()
+    {
+        GridItemsProvider = async request =>
+        {
+            ICollection<RelationResult> r = await RelationService.GetAllAsync().ConfigureAwait(false);
+            return GridItemsProviderResult.From<RelationResult>(r, r.Count);
+        };
+    }
 }
